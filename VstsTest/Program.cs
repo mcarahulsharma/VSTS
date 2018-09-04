ad64db84cb5fc8a5954bc315d118698d5a1fec51
Rahul Sharma <mcarahulsharma@gmail.com>
2018-09-04 10:53:00 +0530

    test change

--- VstsTest/Program.cs
+++ VstsTest/Program.cs
@@ -6,7 +6,7 @@ namespace VstsTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello VSTS");
+            Console.WriteLine("Hello VSTS, is now chaged.");
             Console.Read();
         }
     }
